95d4d2e84197e3b2d9759d5b5a07d50897cc6316
Ytalo Sauham <ytalo.sauhan@hotmail.com>
2021-11-16 19:59:33 -0300

    Logaut inicial habilitado

--- src/components/templatate/MenuLateral.tsx
+++ src/components/templatate/MenuLateral.tsx
@@ -1,4 +1,4 @@
-import { iconeAjustes, iconeCasa, iconeNovidade } from "../icons";
+import { iconeAjustes, iconeCasa, iconeLogaut, iconeNovidade } from "../icons";
 import Logo from "./Logo";
 import MenuItem from "./MenuItem";
 
@@ -7,18 +7,20 @@ import MenuItem from "./MenuItem";
 
 export default function MenuLateral(){
     return(
-        <aside>
+        <aside className={` flex flex-col`}>
             <div className={`
                 flex flex-col items-center justify-center
                 bg-gradient-to-r from-indigo-500  to-purple-800
                 h-20 w-20
             `}><Logo/></div>
-           <ul>
-    
+           <ul className={`flex-grow`}>
             <MenuItem url="/" texto="Inicio" icone={iconeCasa}/>
             <MenuItem url="/ajustes" texto="Ajustes" icone={iconeAjustes}/>
             <MenuItem url="/notificacoes" texto="Notificacao" icone={iconeNovidade}/>
-           
+           </ul>
+           <ul >
+           <MenuItem url="/notificacoes" texto="Sair" icone={iconeLogaut}
+           onClick={() => console.log('logaut')} />
            </ul>
         </aside>
     )
