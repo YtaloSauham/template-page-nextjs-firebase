8c38f478dc78bdce23347268a7c26c85155ac18f
Ytalo Sauham <ytalo.sauhan@hotmail.com>
2021-11-23 13:16:37 -0300

    App context habilitador

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,19 @@
 import Layout from "../components/templatate/Layout";
+// import { AppConsumer } from "../data/context/AppContext";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
+
+  const dados = useAppData()
   return (
     <Layout titulo="Notificacoes" subtitulo="Aqui vao as notificacoes">
-      <h3>CONTEUDO</h3>
+      <h3>CONTEUDO</h3> 
+      {/* <AppConsumer>
+        {dados=><h3>{dados.nome}</h3>}
+      </AppConsumer> */}
+
+      <h3>{dados.nome}</h3> 
+
     </Layout>
   )
 }
