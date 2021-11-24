a141b6230c9921aecc612ba78f8833485a67c5ad
Ytalo Sauham <ytalo.sauhan@hotmail.com>
2021-11-23 22:41:13 -0300

    novas cores de background do botao alternador de tema

--- src/components/templatate/BotaoAlternarTem.tsx
+++ src/components/templatate/BotaoAlternarTem.tsx
@@ -10,13 +10,13 @@ export default function BotaoAlternarTema(props: BotaoAlternarTemaProps){
     return props.tema === 'dark' ? ( 
         <div onClick={props.alternarTema} className={`
         hidden sm:flex items-center cursor-pointer
-        bg-gradient-to-r from-yellow-300 to to-yellow-600
+        bg-gradient-to-r from-yellow-200 to to-yellow-800
         w-14 lg:w-24 h-8 p-1 rounded-full
         
         `}>
             <div className={`
                 flex items-center justify-center
-                bg-white text-yellow-600 w-6 rounded-full
+                bg-transparent text-yellow-600 w-6 rounded-full
             `}>
                 {iconeSol(5)}
             </div>
@@ -31,8 +31,8 @@ export default function BotaoAlternarTema(props: BotaoAlternarTemaProps){
 
     ):(
         <div onClick={props.alternarTema} className={`
-        hidden sm:flex items-center cursor-pointer
-        bg-gradient-to-r from-gray-500 to to-gray-800
+        hidden sm:flex items-center justify-end cursor-pointer
+        bg-gradient-to-r from-gray-500 to to-gray-900
         w-14 lg:w-24 h-8 p-1 rounded-full
         
         `}>
@@ -45,7 +45,7 @@ export default function BotaoAlternarTema(props: BotaoAlternarTemaProps){
             </div>
             <div className={`
                 flex items-center justify-center
-                bg-white text-yellow-600 w-6 rounded-full
+                bg-transparent text-white w-6 rounded-full
             `}>
                 {iconeLua(5)}
             </div>
