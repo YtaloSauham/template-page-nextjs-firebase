c49b83343fbec8e2cbe66d848ad71376f82f3f1f
Ytalo Sauham <ytalo.sauhan@hotmail.com>
2021-12-01 11:36:52 -0300

    componentes de autenticacao implementados

--- src/components/auth/AuthInput.tsx
+++ src/components/auth/AuthInput.tsx
@@ -4,17 +4,20 @@
 interface AuthInputProps {
     label: string
     valor: any
-    type?: 'text' | 'password'
+    obrigatorio?: boolean
+    naoRender?: boolean
+    type?: 'text' |'email' |'password'
     valorMudou: (novoValor: any) => void   
 }
 
 
 export default function AuthInput(props:AuthInputProps){
-    return( 
+    return props.naoRender ? null : ( 
         <div className="flex flex-col">
             <label>{props.label}</label>
-                <input type={props.type} value={props.valor}
+                <input type={props.type ?? 'text'} value={props.valor}
                 onChange={e => props.valorMudou?.(e.target.value)}
+                required={props.obrigatorio}
                 />
             
         </div>
--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -2,23 +2,47 @@ import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
 
 export default function Autenticacao(){
+    const [modo,setModo]= useState<'login' | 'cadastro'>('login')
     const [email, setEmail] = useState('')
     const [password,setPassword] = useState('')
 
+    function submeter(){
+        if(modo === 'login'){
+            console.log('login')
+        } else{
+            console.log('cadastrar')
+        }
+    }
+
+
+
+
+
+
     return (
     <div>
-        <h1>Autenticacao</h1>s
+        <h1 className={`
+        text-xl font-bold mb-5`}>
+            {modo === 'login' ? 'Entre com a Sua Conta' : 'Cadastre-se na Plataforma'}
+        </h1>
         <AuthInput 
         label="Email"
-        type='text'
+        type='email'
         valor={email}
-        valorMudou={setEmail}/>
+        valorMudou={setEmail}
+        obrigatorio
+     />
 
         <AuthInput 
         label="Senha"
         type='password'
         valor={password}
-        valorMudou={setPassword}/>
+        valorMudou={setPassword}
+        obrigatorio/>
+
+        <button onClick={submeter}>
+        {modo === 'login' ? 'Entrar' : 'Cadastrar'}
+        </button>
     </div>
     )
 }
